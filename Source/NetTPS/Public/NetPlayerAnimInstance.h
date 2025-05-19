72568aea68f73e2e216468d98e73c469a11f20e2
GeunEda <144422743+geuneda@users.noreply.github.com>
2025-05-19 12:51:02 +0900

    feat: ABP conduit 설정

---
+++ Source/NetTPS/Public/NetPlayerAnimInstance.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "NetPlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETTPS_API UNetPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	// 총을 소지하고 있는지 여부
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyAnimSettings")
+	bool bHasPistol = false;
+	
+	
+};
